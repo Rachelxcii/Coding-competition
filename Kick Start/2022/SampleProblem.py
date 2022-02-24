2bac2302dbbc8236802385600259e0d6e4b933ab
Rachel Rodríguez <49941851+Rachelxcii@users.noreply.github.com>
2022-02-24 22:15:10 +0000

    Update SampleProblem.py

--- Kick Start/2022/SampleProblem.py
+++ Kick Start/2022/SampleProblem.py
@@ -1,1 +1,24 @@
-#
+# Problem title: Sample Problem
+# Kick Start - Coding Practice 2022
+# Language: Python 3
+# Big-O Notation: O(n)
+
+test = int(input())
+test_case = 1
+
+while test > 0:
+
+    bags, kids = map(int, input().split())
+    candies = list(map(int, input().split()))
+    
+    total_candies = 0
+    
+    for candy in candies:
+        total_candies += candy
+    
+    remained_candies = total_candies % kids
+    
+    print('Case #' + str(test_case) + ': ' + str(remained_candies))
+    
+    test_case += 1
+    test -= 1
